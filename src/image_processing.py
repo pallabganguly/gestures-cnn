5eb750299fdabe5147744335289168880d0bd3f3
Pallab Ganguly <pallabkumarganguly@gmail.com>
2018-05-08 21:01:39 +0530

    changes for HSV color space

--- src/image_processing.py
+++ src/image_processing.py
@@ -20,20 +20,20 @@ gestures = ["index", "fist", "thumb", "vsign"]
 for gesture in gestures:
     os.chdir(PATH+gesture)
     c = 0
-    for image in os.listdir("."):
+    for image in os.listdir(".")[:5]:
         img = cv2.imread(image)
-        crop = img[120:840, 0:720]
-        # crop = img[150:870, 0:720]
-        # hsv = cv2.cvtColor(crop, cv2.COLOR_BGR2HSV)
-        blur = cv2.GaussianBlur(crop, (33, 33), 0)
+        # crop = img[120:840, 0:720]
+        crop = img[150:870, 0:720]
+        hsv = cv2.cvtColor(crop, cv2.COLOR_BGR2HSV)
+        blur = cv2.GaussianBlur(hsv, (33, 33), 0)
         # mask = cv2.inRange(hsv, np.array([0, 5, 3]), np.array([60, 255, 255]))
         # gaussian = cv2.GaussianBlur(mask, (11,11), 0)
-        erosion = cv2.erode(crop, None, iterations = 1)
+        erosion = cv2.erode(blur, None, iterations = 1)
         dilated = cv2.dilate(erosion,None,iterations = 1)
         median = cv2.medianBlur(dilated, 7)
         # res = cv2.resize(median, (30, 30))
-        filename = "/home/pallab/gestures-cnn/images/orig/"+str(gesture[0])+str(c)+".jpg" # changed here
-        # filename = "/home/pallab/Desktop/orig/"+str(gesture[0])+str(c)+".jpg"
+        # filename = "/home/pallab/gestures-cnn/images/orig/"+str(gesture[0])+str(c)+".jpg" # changed here
+        filename = "/home/pallab/Desktop/orig/"+str(gesture[0])+str(c)+".jpg"
         # print(c)
         cv2.imwrite(filename, median)
         c += 1
